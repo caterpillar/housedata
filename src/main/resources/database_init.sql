c2eb234192a8d9090d478b936b369762f8058cc2
lishaohua <li_ye_hua@126.com>
2017-03-26 11:34:39 +0800

    modify sql

--- src/main/resources/database_init.sql
+++ src/main/resources/database_init.sql
@@ -16,20 +16,17 @@ CREATE TABLE `city_config` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-
 CREATE TABLE `house_data` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `city_id` varchar(20) NOT NULL,
   `city_name` varchar(30) NOT NULL,
   `title` varchar(50) DEFAULT NULL,
   `area` varchar(20) DEFAULT NULL,
-  `section` VARCHAR(20) DEFAULT NULL,
-  `address` VARCHAR(100) DEFAULT NULL,
-  `sale_status` VARCHAR(10) DEFAULT NULL,
-  `origin_url` VARCHAR(200) DEFAULT NULL,
+  `section` varchar(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `sale_status` varchar(10) DEFAULT NULL,
+  `origin_url` varchar(200) DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 
